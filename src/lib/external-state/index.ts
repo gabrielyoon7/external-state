f06272ecc1017c8ec656ab9595dd6c2b03db4c63
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-07-04 12:15:27 +0900

    chore: 빌드 설정 추가

--- src/lib/external-state/index.ts
+++ src/lib/external-state/index.ts
@@ -2,21 +2,28 @@ import { DataObserver } from "./types.ts";
 import { useSyncExternalStore } from "react";
 import StateManager from "./StateManager.ts";
 
-export const useExternalState = <T>(store: DataObserver<T>): [T, (newState: T) => void] => {
+const useExternalState = <T>(store: DataObserver<T>): [T, (newState: T) => void] => {
   const state = useSyncExternalStore(store.subscribe, store.getState);
 
   return [state, store.setState];
 };
-export const useSetExternalState = <T>(store: DataObserver<T>): (newState: T) => void => {
+const useSetExternalState = <T>(store: DataObserver<T>): (newState: T) => void => {
   return store.setState;
 };
-export const useExternalValue = <T>(store: DataObserver<T>): T => {
+const useExternalValue = <T>(store: DataObserver<T>): T => {
   const state = useSyncExternalStore(store.subscribe, store.getState);
 
   return state;
 };
 
-export const store = <T>(initialState: T) => {
+const store = <T>(initialState: T) => {
   const stateManager = new StateManager<T>(initialState);
   return stateManager;
 };
+
+export {
+  useExternalState,
+  useSetExternalState,
+  useExternalValue,
+  store
+}
