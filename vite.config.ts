a9aedfcd965dd459bb48dcf8a1f28e0fced8bd1f
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-11-07 01:29:07 +0900

    fix: github pages 배포 제거

--- vite.config.ts
+++ vite.config.ts
@@ -3,30 +3,26 @@ import react from '@vitejs/plugin-react'
 import dts from "vite-plugin-dts";
 import * as path from "path";
 
-const isProduction = process.env.NODE_ENV === 'production';
-
-const buildOptions = isProduction && {
-  lib: {
-    entry: path.resolve(__dirname, "src/lib/index.tsx"),
-    name: "index",
-    fileName: "index",
-  },
-  rollupOptions: {
-    external: ["react"],
-    output: {
-      globals: {
-        react: "React",
-      },
-    },
-  },
-  commonjsOptions: {
-    esmExternals: ["react"],
-  },
-}
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), isProduction && dts()],
+  plugins: [react(), dts()],
   base: "/external-state/",
-  build: buildOptions,
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/lib/index.tsx"),
+      name: "index",
+      fileName: "index",
+    },
+    rollupOptions: {
+      external: ["react"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+    commonjsOptions: {
+      esmExternals: ["react"],
+    },
+  },
 })
