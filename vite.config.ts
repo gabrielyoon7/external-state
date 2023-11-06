1716b960ea287309af058c2f70d8df08f2eacabc
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-11-07 01:16:38 +0900

    chore: github pages 배포 시, 라이브러리가 아닌 예제 페이지가 배포 되도록 수정

--- vite.config.ts
+++ vite.config.ts
@@ -3,26 +3,30 @@ import react from '@vitejs/plugin-react'
 import dts from "vite-plugin-dts";
 import * as path from "path";
 
+const isProduction = process.env.NODE_ENV === 'production';
+
+const buildOptions = isProduction && {
+  lib: {
+    entry: path.resolve(__dirname, "src/lib/index.tsx"),
+    name: "index",
+    fileName: "index",
+  },
+  rollupOptions: {
+    external: ["react"],
+    output: {
+      globals: {
+        react: "React",
+      },
+    },
+  },
+  commonjsOptions: {
+    esmExternals: ["react"],
+  },
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), dts()],
   base: "/external-state/",
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, "src/lib/index.tsx"),
-      name: "index",
-      fileName: "index",
-    },
-    rollupOptions: {
-      external: ["react"],
-      output: {
-        globals: {
-          react: "React",
-        },
-      },
-    },
-    commonjsOptions: {
-      esmExternals: ["react"],
-    },
-  },
+  build: buildOptions,
 })
